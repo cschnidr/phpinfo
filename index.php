6c729f397434dcad5310361f621d11f36a1e8fe1
cschnidr <cschnidr@netapp.com>
2017-11-24 11:47:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
-<?php
-echo ' 
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
-  <title>Running on POD $_ENV['HOSTNAME']</title>
+  <?php
+    echo "<title> Running on POD ".$_ENV['HOSTNAME']." </title>\n";
+   ?>
 </head>
 <img src="/web/thePub_GreyBlue.png"/>
 </html>
-';
+
+<?php
 phpinfo();
 ?>
