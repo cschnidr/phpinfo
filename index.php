509737ca1dfbdc5293d47e00be7b29fd510bd2a7
cschnidr <cschnidr@netapp.com>
2017-11-24 11:33:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ echo '
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
-  <title>"Hello ".$_ENV['HOSTNAME']." NetApp muäsch ha !\n"</title>
+  <title>Hello .$_ENV['HOSTNAME']. NetApp muäsch ha !\n</title>
 </head>
 <img src="/web/thePub_GreyBlue.png"/>
 </html>
